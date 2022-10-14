f9cfefb5e9fe74976057d9b007dc4d4833e7c13a
Sandeep singh <sandy180388@gmail.com>
2022-10-14 12:13:23 +0530

    Main

---
+++ Array_Problems/Repeated_Digit_Between_1-100.js
@@ -0,0 +1,11 @@
+//program to find the elements repeated digit using palindrome and print the array
+function isRepeated(number) {
+  if (number % 11 == 0) return true;
+  else return false;
+}
+
+let array = new Array();
+for (let i = 1; i < 100; i++) {
+  if (isRepeated(i)) array.push(i);
+}
+console.log(array);
---
+++ Array_Problems/SortArray.js
@@ -0,0 +1,30 @@
+//get 10 random 3 digits number and print 2nd largest and 2nd smallest 
+//array constructor
+let numArray = new Array(10);
+for(let i = 0; i<numArray.length; i++)
+{
+    //generates 10random 3 digit number
+    numArray[i]=Math.floor(Math.random()*900)+100;
+}
+console.log(numArray);
+//traversing through for loop to sort array
+for(let i = 0; i<numArray.length; i++)
+{
+    for(let j = 0; j<numArray.length - 1; j++)
+    {
+        if(numArray[j]>numArray[j+1])
+        {
+            let temp = numArray[j];
+            numArray[j]=numArray[j+1];
+            numArray[j+1]=temp;
+        }
+    }
+}
+//print statements
+console.log(numArray);
+console.log("Second largest element: "+numArray[(numArray.length-2)]+"\nSecond smallest element: "+numArray[1]);
+
+//To sort Array
+let sortedArray = numArray.sort();
+console.log("Sorted Array is: ");
+console.log("By sorting \n Second largest elemet: "+sortedArray[(sortedArray.length-2)]+"\nSecond smallest element: "+sortedArray[1]);
